3f11dbf5bc6bb38a0af1cbfca3b7e43160ddbcba
Rajesh E. <7800366@indus.local>
2025-07-25 17:03:31 +0530

    cleanup default vite files and update app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
+import SignInSignUp from './components/SignInSignUp'
 
 const App = () => {
   return (
-    <div className='bg-deepblue container'>
-      <h1 className='text-5xl font-bold text-white'>Tailwind CSS</h1>
-      <button className='bg-amber text-alabaster border-4 text-2xl font-bold mt-12 rounded px-6 py-4'>Click here!</button>
-      <input type="text" placeholder='Enter your name' className='bg-white text-lg text-amber border-2 p-4 
-      rounded ml-4 outline-0' />
+    <div>
+      <SignInSignUp />
     </div>
-    
   )
 }
 
